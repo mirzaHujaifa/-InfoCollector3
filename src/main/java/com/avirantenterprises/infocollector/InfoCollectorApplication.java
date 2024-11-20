110a534d219e8b28bca3ffd5e8cb024df109088e
Mirza <mirzahujaifa778@gmail.com>
2024-11-20 23:34:51 +0530

    Initial commet

---
+++ src/main/java/com/avirantenterprises/infocollector/InfoCollectorApplication.java
@@ -0,0 +1,14 @@
+package com.avirantenterprises.infocollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.Desktop;
+@SpringBootApplication
+public class InfoCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoCollectorApplication.class, args);
+	}
+
+}
